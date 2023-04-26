dbcfe7127c6fab6e81e42326233a9b89e6bc2f33
mjd10m <mjd10m@outlook.com>
2023-04-25 21:47:46 -0400

    Contact form component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About'
 import Nav from './components/Nav'
 import Gallery from './components/Gallery'
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -26,10 +27,11 @@ const [currentCategory, setCurrentCategory] = useState(categories[0])
       categories = {categories}
       setCurrentCategory = {setCurrentCategory}
       currentCategory = {currentCategory}
-      ></Nav>
+      />
       <main>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        <ContactForm/>
+        <Gallery currentCategory={currentCategory}/>
+        <About/>
       </main>
 
     </div>
---
+++ src/components/Contact/index.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+
+function ContactForm () {
+    return(
+        <section>
+            <h1>Contact me</h1>
+            <form id='contact-form'>
+                <div>
+                    <label htmlFor='name'>Name:</label>
+                    <input type='text' name='name'/>
+                </div>
+                <div>
+                    <label htmlFor='email'>Email Address:</label>
+                    <input type='email' name='email'/>
+                </div>
+                <div>
+                    <label htmlFor='name'>Message:</label>
+                    <textarea name='name' rows='5'/>
+                </div>
+                <button type='submit'>Submit</button>
+            </form>
+        </section>
+    )
+}
+
+export default ContactForm
